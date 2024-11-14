dfb69513d1eafeafb23c9963599474d8a03d319d
binhminh <binhminh.vg@gmail.com>
2024-11-14 21:29:56 +0700

    final

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -21,7 +21,7 @@ export class PaymentComponent implements OnInit {
   paymentDto!: PaymentDto;
   loadingQrCode = true; // Loading state for QR code
   qrCodeUrl!: SafeUrl; // Safe URL for QR code
-  paymentProcessing: boolean = false;
+  paymentProcessing: boolean = true;
   cities: any[] = [];
   districts: any[] = [];
 
@@ -51,133 +51,144 @@ export class PaymentComponent implements OnInit {
       await this.fetchOrderDetails();
       this.fetchQrCode(); // Fetch QR code for payment
     });
+
+    setTimeout(() => {
+      this.paymentProcessing = false;
+    }, 10000);
+
   }
 
-  calculateTotalPrice(order: OrderWithDetail): number {
-    var totalPrice = 0;
-    for (let orderDetail of order.orderDetails) {
+
+
+
+calculateTotalPrice(order: OrderWithDetail): number {
+  var totalPrice = 0;
+  for (let orderDetail of order.orderDetails) {
+    totalPrice += (orderDetail.priceProduc || 0) * (orderDetail.quantity || 0);
+  }
+  return totalPrice;
+}
+
+
+
+fetchOrderDetails(): Promise < void> {
+  return new Promise((resolve, reject) => {
+    this.orderService.getOrderById(this.orderId).subscribe({
+      next: (order) => {
+        this.order = order;
+        this.ndck = order.no_;
+        var orderAmount = 0;
+        for (let orderDetail of this.order.orderDetails) {
+          orderAmount += (orderDetail.priceProduc || 0) * (orderDetail.quantity || 0);
+        }
+        this.amount = String(orderAmount);
+        console.log("amount:" + this.amount);
+        this.initializePayment();
+        resolve();
+      },
+      error: (error) => {
+        console.error("Error fetching order details:", error);
+        reject(error);
+      }
+    });
+  });
+}
+
+
+initializePayment() {
+  if (this.order && this.order.orderDetails) {
+    let totalPrice = 0;
+
+    for (let orderDetail of this.order.orderDetails) {
       totalPrice += (orderDetail.priceProduc || 0) * (orderDetail.quantity || 0);
     }
-    return totalPrice;
-  }
 
-  fetchOrderDetails(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.orderService.getOrderById(this.orderId).subscribe({
-        next: (order) => {
-          this.order = order;
-          this.ndck = order.no_;
-          var orderAmount = 0; 
-          for (let orderDetail of this.order.orderDetails) {
-            orderAmount += (orderDetail.priceProduc || 0) * (orderDetail.quantity || 0);
-          }
-          this.amount = String(orderAmount);
-          console.log("amount:" + this.amount);
-          this.initializePayment();
-          resolve();
-        },
-        error: (error) => {
-          console.error("Error fetching order details:", error);
-          reject(error);
-        }
-      });
-    });
-  }
+    this.paymentDto = {
+      id: 0, // Backend generates ID
+      userId: this.order.userID ?? 0,
+      orderId: this.order.id ?? 0,
+      method: "BANKTRANSFER",
+      totalPrice: totalPrice,
+      status: 'Pending',
+      createdTime: new Date(),
+      updatedTime: new Date()
+    };
 
-  initializePayment() {
-    if (this.order && this.order.orderDetails) {
-      let totalPrice = 0;
-
-      for (let orderDetail of this.order.orderDetails) {
-        totalPrice += (orderDetail.priceProduc || 0) * (orderDetail.quantity || 0);
-      }
-    
-      this.paymentDto = {
-        id: 0, // Backend generates ID
-        userId: this.order.userID ?? 0,
-        orderId: this.order.id ?? 0,
-        method: "BANKTRANSFER",
-        totalPrice: totalPrice,
-        status: 'Pending',
-        createdTime: new Date(),
-        updatedTime: new Date()
-      };
-
-      this.paymentService.createPayment(this.paymentDto).subscribe({
-        next: response => {
-          this.paymentDto = response;
-          console.log('Payment processed successfully:', response);
-        },
-        error: error => {
-          console.error('Payment processing failed:', error);
-        }
-      });
-    } else {
-      console.error('Order details are missing');
-    }
-  }
-
-  fetchQrCode(): void {
-    console.log("Amount: " + this.amount);
-    var qrUrl = this.qrService.getQrCode(this.bank, this.accountNumber, this.amount, this.ndck, this.fullName);
-    this.qrCodeUrl = this.sanitizer.bypassSecurityTrustUrl(qrUrl);
-    this.loadingQrCode = false;
-  }
-
-  extractAddressComponents(address: string): { cityId: string, districtId: string } {
-    const parts = address.split(',').map(part => part.trim());
-    
-    const cityName = parts.pop() || '';        // Extract the last part as the city
-    const districtName = parts.pop() || '';     // Extract the second last part as the district
-    
-    const city = this.cities.find(city => city.name === cityName);          // Find city ID
-    const district = this.cities.find(district => district.name === districtName);  // Find district ID
-  
-    return { cityId: city ? city.id : "100000", districtId: district ? district.id : "100900" };
-  }
-  
-
-  loadLocations(): void {
-    // Load cities
-    this.http.get<any[]>('/assets/statics/cities.json').subscribe(data => {
-      this.cities = data;
-    });
-
-    // Load districts
-    this.http.get<any[]>('/assets/statics/districts.json').subscribe(data => {
-      this.districts = data;
-    });
-  }
-
-  onSubmitPayment() {
-    console.log(this.paymentDto);
-    this.paymentService.confirmPayment(this.paymentDto.id).subscribe({
+    this.paymentService.createPayment(this.paymentDto).subscribe({
       next: response => {
-        console.log('Payment confirmed successfully:', response);
-        this.paymentProcessing = true;
-        var { cityId, districtId } = this.extractAddressComponents(this.order.shipAddress);
-
-
-        // Call createDelivery and wait for it to complete before navigating
-        this.deliveryService.createDelivery(this.paymentDto, cityId, districtId).subscribe({
-          next: (deliveryResponse: any) => {
-            console.log('Delivery created successfully:', deliveryResponse);
-            this.router.navigate([`/payment/${this.paymentDto.orderId}/confirmed`]);
-          },
-          error: (deliveryError: any) => {
-            console.error('Delivery creation failed:', deliveryError);
-          }
-        });
+        this.paymentDto = response;
+        console.log('Payment processed successfully:', response);
       },
       error: error => {
         console.error('Payment processing failed:', error);
       }
     });
+  } else {
+    console.error('Order details are missing');
   }
+}
+
+fetchQrCode(): void {
+  console.log("Amount: " + this.amount);
+  var qrUrl = this.qrService.getQrCode(this.bank, this.accountNumber, this.amount, this.ndck, this.fullName);
+  this.qrCodeUrl = this.sanitizer.bypassSecurityTrustUrl(qrUrl);
+  this.loadingQrCode = false;
+}
+
+extractAddressComponents(address: string): { cityId: string, districtId: string } {
+  const parts = address.split(',').map(part => part.trim());
+
+  const cityName = parts.pop() || '';        // Extract the last part as the city
+  const districtName = parts.pop() || '';     // Extract the second last part as the district
+
+  const city = this.cities.find(city => city.name === cityName);          // Find city ID
+  const district = this.cities.find(district => district.name === districtName);  // Find district ID
+
+  return { cityId: city ? city.id : "100000", districtId: district ? district.id : "100900" };
+}
 
 
-  getImageUrl(productID: number) {
-    const HostUrl = "https://localhost:5001/api";
-    return productID ? `${HostUrl}/Products/images/product/${productID}` : '';
-  }
+loadLocations(): void {
+  // Load cities
+  this.http.get<any[]>('/assets/statics/cities.json').subscribe(data => {
+    this.cities = data;
+  });
+
+  // Load districts
+  this.http.get<any[]>('/assets/statics/districts.json').subscribe(data => {
+    this.districts = data;
+  });
+}
+
+onSubmitPayment() {
+  console.log(this.paymentDto);
+  this.paymentService.confirmPayment(this.paymentDto.id).subscribe({
+    next: response => {
+      console.log('Payment confirmed successfully:', response);
+      this.paymentProcessing = true;
+      var { cityId, districtId } = this.extractAddressComponents(this.order.shipAddress);
+
+
+      // Call createDelivery and wait for it to complete before navigating
+      this.deliveryService.createDelivery(this.paymentDto, cityId, districtId).subscribe({
+        next: (deliveryResponse: any) => {
+          console.log('Delivery created successfully:', deliveryResponse);
+          this.router.navigate([`/payment/${this.paymentDto.orderId}/confirmed`]);
+        },
+        error: (deliveryError: any) => {
+          console.error('Delivery creation failed:', deliveryError);
+        }
+      });
+    },
+    error: error => {
+      console.error('Payment processing failed:', error);
+    }
+  });
+}
+
+
+getImageUrl(productID: number) {
+  const HostUrl = "https://localhost:5001/api";
+  return productID ? `${HostUrl}/Products/images/product/${productID}` : '';
+}
 }
